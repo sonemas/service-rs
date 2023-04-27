2e0579d145352d73fd1fcbbf39de671078a506ef
Arjan van Eersel <arjan@sonemas.com>
2023-04-27 10:15:10 +0300

    Create tests and documentation for key module

--- foundation/src/key.rs
+++ foundation/src/key.rs
@@ -1,3 +1,4 @@
+//! Provides functionality for signing keys.
 use std::{error::Error, fmt, fs::{File, self}, io::{Write, self}};
 
 use ring::{
@@ -5,13 +6,17 @@ use ring::{
     signature::{self, KeyPair}, error,
 };
 
+/// A trait that all signing keys need to implement.
 pub trait SigningKey {
-    fn sign(&self, message: &[u8]) -> Result<signature::Signature, KeyError>;
-    fn verify_signature(message: &[u8], signature: &[u8]) -> bool;
+    /// Signs the provided message and returns the signature or a KeyError.
+    fn sign(&self, message: &[u8]) -> Result<Vec<u8>, KeyError>;
+
+    /// Verifies whether the message was signed by the signing key instance.
     fn has_signed(&self, message: &[u8], signature: &[u8]) -> bool;
 }
 
 #[derive(Debug)]
+/// Custom errors for this package.
 pub enum KeyError {
     RingUnspecifiedError(error::Unspecified),
     RingKeyRejected(error::KeyRejected),
@@ -51,42 +56,52 @@ impl fmt::Display for KeyError {
 
 impl Error for KeyError {}
 
+/// A default signing key implementation using the Ed25519 algorithm.
 pub struct Key {
     der_bytes: Vec<u8>,
 }
 
 impl Key{
+    /// Returnes a newly initialized key.
     pub fn new() -> Result<Self, KeyError>  {
         let rng = rand::SystemRandom::new();
-        let alg = &signature::ECDSA_P256_SHA256_ASN1_SIGNING;
-        let der_bytes = signature::EcdsaKeyPair::generate_pkcs8(alg, &rng)?.as_ref().to_vec();
+        let der_bytes = signature::Ed25519KeyPair::generate_pkcs8(&rng)?.as_ref().to_owned();
 
-        Ok(Self{der_bytes})
+        Ok(der_bytes.into())
     }
 
+    /// Returns a key from a .der file.
     pub fn open(filename: &str) -> Result<Self, KeyError> {
         let der_bytes = fs::read(filename)?[..].to_vec();
-        Ok(Self{der_bytes})
+        Ok(der_bytes.into())
     }
 
+    /// Saves a key as a .der file.
     pub fn save(&self, filename: &str) -> io::Result<()> {
         File::create(filename)?.write_all(self.der_bytes.as_ref())
     }
 }
 
+impl From<&[u8]> for Key {
+    fn from(der_bytes: &[u8]) -> Self {
+        Self{der_bytes: der_bytes.to_vec()}
+    }
+}
+
+impl From<Vec<u8>> for Key {
+    fn from(der_bytes: Vec<u8>) -> Self {
+        Self{der_bytes}
+    }
+}
+
 impl SigningKey for Key {
-    fn sign(&self, message: &[u8]) -> Result<signature::Signature, KeyError> {
+    /// Signs the provided message and returns the signature or a KeyError.
+    fn sign(&self, message: &[u8]) -> Result<Vec<u8>, KeyError> {
         let key_pair = signature::Ed25519KeyPair::from_pkcs8(self.der_bytes.as_ref())?;
-        Ok(key_pair.sign(message))
+        Ok(key_pair.sign(message).as_ref().to_vec())
     }
 
-    fn verify_signature(message: &[u8], signature: &[u8]) -> bool {
-        let public_key = 
-            signature::UnparsedPublicKey::new(&signature::ECDSA_P256_SHA256_ASN1, signature);
-        
-        public_key.verify(message, signature).is_ok()
-    }
-
+    /// Verifies whether the message was signed by the signing key instance.
     fn has_signed(&self, message: &[u8], signature: &[u8]) -> bool {
         let key_pair = match signature::Ed25519KeyPair::from_pkcs8(self.der_bytes.as_ref()) {
             Ok(v) => v,
@@ -94,7 +109,7 @@ impl SigningKey for Key {
         };
         
         let public_key =
-            signature::UnparsedPublicKey::new(&signature::ECDSA_P256_SHA256_ASN1, key_pair.public_key().as_ref());
+            signature::UnparsedPublicKey::new(&signature::ED25519, key_pair.public_key().as_ref());
 
         public_key.verify(message, signature.as_ref()).is_ok()
     }
@@ -105,8 +120,26 @@ mod test {
     use super::*;
 
     #[test]
-    fn it_works() {
-        todo!("Implement tests and comments")
+    fn it_can_sign_and_verify() {
+        const MESSAGE: &[u8] = b"This is a test message";
+        let key = Key::new().expect("should be able to create new key");
+
+        let signature = key.sign(MESSAGE).expect("should be able to sign message");
+
+        assert!(key.has_signed(MESSAGE, &signature));
     }
 
+    #[test]
+    fn it_can_save_and_load_keys_from_file() {
+        const MESSAGE: &[u8] = b"This is a test message";
+        let orig_key = Key::new().expect("should be able to create new key");
+        orig_key.save("/tmp/testkey.der").expect("should be able to save key file");
+
+        let key = Key::open("/tmp/testkey.der").expect("should be able to load key file");
+
+        let signature = key.sign(MESSAGE).expect("should be able to sign message");
+
+        assert!(key.has_signed(MESSAGE, &signature));
+        _ = std::fs::remove_file("/tmp/testkey.der");
+    }
 }
