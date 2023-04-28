1d0bfe4eff024b3848e1d4e57104f614cc94dd89
Arjan van Eersel <arjan@sonemas.com>
2023-04-28 11:12:00 +0300

    Implement custom id and user service

---
+++ foundation/src/id.rs
@@ -0,0 +1,25 @@
+use std::fmt::Display;
+
+use serde::{Serialize, Deserialize};
+use uuid::Uuid;
+
+#[derive(Serialize, Deserialize, Debug, PartialEq, Clone)]
+pub struct Id(String);
+
+impl Id {
+    pub fn new() -> Self {
+        Id(Uuid::new_v4().to_string())
+    }
+}
+
+impl Display for Id {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self.0)
+    }
+}
+
+impl From<&str> for Id {
+    fn from(value: &str) -> Self {
+        Id(value.to_string())
+    }
+}
